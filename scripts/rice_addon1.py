2d7e093624ceda739ad3bc32776ac7b4fda7f0ca
siantist <104287799+siantist@users.noreply.github.com>
2023-04-03 22:21:45 -0700

    Create rice_addon1.py

---
+++ scripts/rice_addon1.py
@@ -0,0 +1,24 @@
+
+def general_attention(self):
+    # encoder representation of different words
+    
+
+
+
+
+# https://medium.com/hackernoon/building-a-feedforward-neural-network-from-scratch-in-python-d3526457156b
+# build weight matrix 
+def attention_mechanism(self):
+    pai = 1/(len(self/actions_nvec)) # P(A_i)
+    
+    from tf.keras import Sequential
+    from tf.keras.layers import Dense
+    
+    nlen = len(self.relevant_actions)
+    # define a, feedforward network
+    a_model = Sequential() # NN()
+    a_model.add(Dense())
+    model.add(Dense(10, activation="softmax"))
+    np.dot()
+    # a is the identity 
+    res= a(dot_prod)
